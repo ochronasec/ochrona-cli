d033f0b5e51ac50a167c834d2f4ecd2e7f5f9d26
ochronasec <andrew@ochrona.dev>
2021-03-20 07:37:43 -0400

    unit tests

--- tests/test_import_wrapper.py
+++ tests/test_import_wrapper.py
@@ -143,7 +143,7 @@ class TestImportWrapper:
                     "Click==7.0",
                     "Flask==1.1.1",
                     "itsdangerous==1.1.0",
-                    "Jinja2==2.10.1",
+                    "Jinja2==2.11.3",
                     "MarkupSafe==1.1.1",
                     "Werkzeug==0.15.4",
                 ],
